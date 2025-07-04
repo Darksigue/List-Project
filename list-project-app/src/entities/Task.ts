d2ed7198084923cd1b33cc4dda63c11ca5104cb2
João Paulo Akira Sigue <joaopsigue7@gmail.com>
2025-07-04 00:41:57 -0300

    fix: Task build

--- list-project-app/src/entities/Task.ts
+++ list-project-app/src/entities/Task.ts
@@ -7,9 +7,9 @@ export class Task implements TaskInterface {
     description?: string;
     status: string;
 
-    createTask(title: string, status: string, description?: string): string {
+    async createTask(title: string, status: string, description?: string) {
         try {
-            prisma.task.create({
+            await prisma.task.create({
                 data: {
                     title: title,
                     description: description,
@@ -23,23 +23,74 @@ export class Task implements TaskInterface {
         }
     }
 
-    getTask(): Promise<TaskInterface | void> {
+    getId() {
+        return this.id;
+    }
+
+    setId(id: number) {
+        this.id = id;
         return;
     }
 
-    getTasks(): Promise<TaskInterface[] | void> {
+    async getTask(id: number) {
+
+        try {
+
+            const task = prisma.task.findUnique({
+                where: {
+                    id: id | this.id
+                }
+            })
+
+            return task;
+
+        } catch (e) {
+            throw new Error("Erro ao buscar atividade")
+        }
+
+    }
+
+    async getTasks() {
+        try {
+            const tasks = prisma.task.findMany();
+
+            return tasks;
+        } catch (e) {
+            throw new Error("Erro ao buscar atividades")
+        }
+
+    }
+
+    async updateTask(id: number, body: TaskBody) {
+        try {
+            await prisma.task.findUnique({
+                where: {
+                    id: id,
+                    title: body.title,
+                    description: body.description,
+                    status: body.status
+                },
+            })
+            return "Atividade atualizada com sucesso"
+
+        } catch (e) {
+            console.log(e);
+            throw new Error("Erro ao atualizar atividade");
+        }
+    }
+
+    setTitle(title: string) {
+        this.title = title;
         return;
     }
 
-    setTitle(): void {
+    setDescription(description: string) {
+        this.description = description;
         return;
     }
 
-    setDescription(): void {
-        return;
-    }
-
-    setStatus(): void {
+    setStatus(status: string) {
+        this.status = status;
         return;
     }
 }
--- list-project-app/src/interfaces/Task.ts
+++ list-project-app/src/interfaces/Task.ts
@@ -4,10 +4,20 @@ interface TaskInterface {
     description?: string;
     status: string;
 
-    createTask(title: string, status: string, description?: string): string;
-    getTask(): Promise<TaskInterface | void>;
-    getTasks(): Promise<TaskInterface[] | void>;
-    setTitle(): void;
-    setDescription(): void;
-    setStatus(): void;
+    createTask(title: string, status: string, description?: string): Promise<string>;
+    getId(): number;
+    setId(id: number): void;
+    getTask(id: number): unknown;
+    getTasks(): unknown;
+    updateTask(id: number, body: TaskBody): void;
+    setTitle(title: string): void;
+    setDescription(description: string): void;
+    setStatus(status: string): void;
+}
+
+interface TaskBody {
+    id: number;
+    title: string;
+    description?: string;
+    status: string;
 }
